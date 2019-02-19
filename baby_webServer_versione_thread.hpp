1e89371d19fb3385d95349b84b70603137128277
FilippoMartino <43878495+FilippoMartino@users.noreply.github.com>
2019-02-19 14:05:53 +0100

    Aggiornamento baby web server ottimizzato
    Ottimizzazione del codice precedente, con la gestione di un sistema multi-thread
    Aggiunta di commenti al codice

--- baby_webServer_versione_thread.hpp
+++ baby_webServer_versione_thread.hpp
@@ -1,27 +1,82 @@
-#include "socket_tcp"
+#include "socket_tcp.hpp"
 
-void request(void* connection);
+//Funzione thread
+void request(void* params);
+
+typedef struct {
+
+		ConnServer* conn;
+		ServerTcp* myself;
+
+} Params;
 
 int main (int argc, char const *argv[]){
   
   int port = atoi(argv[1]);
-  char* home = HOME_PAGE;
-  
+	
+		// Idea di Wang, far diventare questo un SINGLETON (DP GOF)
   ServerTcp* myself = new ServerTcp(port);
   
   while(true){
     
     ConnServer* conn = myself->accetta();
-    pthread_t
+    /*
+    
+    		Dobbiamo mallocare la uno spazio in memoria
+    		con i parametri in modo che sia sempre visibile
+    		al thread cui dovrà lavorarci, altrimenti verrebbe
+    		deallocata alla fine del while.
+    		Così facendo alloco uno spazio nello heap, lo faccio
+    		puntare da una variabile e passo la stessa alla funzione
+    		pthread_create, che la utilizzerà per il thread stesso.
+    		Alla fine del ciclo il puntatore verrà deallocato, senza
+    		toccare i parametri nello heap che verranno poi eliminati 
+    		dal thread dopo averne prelevato i valori.
+    		
+    */
+				Params* params = (Params*) malloc(sizeof(Params));
+				// Assegno i valori all'area di memoria
+				params->connection = conn;
+				params->myslef = myself;
+				// Alloco il thread_id
+    pthread_t thread_id;
+    // Creo il thread che verrà messo in coda dal gestore di processi
+    pthread_create(&thread_id,
+    															NULL,
+    															request, // <- funzione da chiamare
+    															(void*) params; //	<- struttura con dati da passare alla funzione
   
   }  
   
+  delete(myself);
+  
+  return 0;
+  
 }
 
 
-void* request(void connection) {
+void* request(void* params) {
   
-  ConnServer * conn = (ConnServer*) connection;
-  conn->invia(HOME_PAGE);
+  // Mi preno la risposta da fornire al client
+  static char* home = HOME_PAGE;
+  
+  // Lo si fa per il casting
+  Params* p = (Params) params;
+  // Salvo i valori puntati in due variabili locali
+  ConnServer* conn = (ConnServer*) p->connection;
+  ServerTcp* myself = (ConnServer*) p->connection;
+  // elimino i parametri dallo heap
+  free(p);
+  // Ricevo le richieste
+  conn->ricevi;
+  // Invio le risposte
+  conn->invia(home);
+		// Disconnetto questa conn dalla lista  
+  myself->disconnetti();
+  // Termino il thread
+  pthread_exit(NULL); 
   
 }
+
+
+
