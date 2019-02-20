616ffc4552c9cabd299a49ef47ba2059cbcd1a56
filippomartino <filmartino12@gmail.com>
2019-02-20 16:28:54 +0100

    prova collegamento da macchina virtuale

--- baby_webServer_versione_thread.hpp
+++ baby_webServer_versione_thread.hpp
@@ -14,16 +14,17 @@ Content-Type: text/html; charset=UTF-8\n\
 
 typedef struct {
 
+		//oke
 		ConnServer* conn;
 		ServerTCP* myself;
 
 } Params;
 
-void* request(void* params) {	
-  
+void* request(void* params) {
+
   // Mi prendo la risposta da fornire al client
   static char* home = strdup("foo");
-  
+
   // Lo si fa per il casting
   Params* p = (Params*) params;
   // Salvo i valori puntati in due variabili locali
@@ -35,25 +36,25 @@ void* request(void* params) {
   conn->ricevi();
   // Invio le risposte
   conn->invia(home);
-		// Disconnetto questa conn dalla lista  
+		// Disconnetto questa conn dalla lista
   myself->disconnetti(conn);
   // Termino il threadz
-  pthread_exit(NULL); 
-  
+  pthread_exit(NULL);
+
 }
 
 int main (int argc, char const *argv[]){
-  
+
   int port = atoi(argv[1]);
-	
+
 		// Idea di Wang, far diventare questo un SINGLETON (DP GOF)
   ServerTCP* myself = new ServerTCP(port);
-  
+
   while(true){
-    
+
     ConnServer* conn = myself->accetta();
     /*
-    
+
     		Dobbiamo mallocare la uno spazio in memoria
     		con i parametri in modo che sia sempre visibile
     		al thread cui dovrà lavorarci, altrimenti verrebbe
@@ -62,9 +63,9 @@ int main (int argc, char const *argv[]){
     		puntare da una variabile e passo la stessa alla funzione
     		pthread_create, che la utilizzerà per il thread stesso.
     		Alla fine del ciclo il puntatore verrà deallocato, senza
-    		toccare i parametri nello heap che verranno poi eliminati 
+    		toccare i parametri nello heap che verranno poi eliminati
     		dal thread dopo averne prelevato i valori.
-    		
+
     */
 				Params* params = (Params*) malloc(sizeof(Params));
 				// Assegno i valori all'area di memoria
@@ -73,18 +74,12 @@ int main (int argc, char const *argv[]){
 				// Alloco il thread_id
     pthread_t thread_id;
     // Creo il thread che verrà messo in coda dal gestore di processi
-    pthread_create(&thread_id, NULL, request,	 (void*) params); 
-  
-  }  
-  
+    pthread_create(&thread_id, NULL, request,	 (void*) params);
+
+  }
+
   delete(myself);
-  
+
   return 0;
-  
+
 }
-
-
-
-
-
-
